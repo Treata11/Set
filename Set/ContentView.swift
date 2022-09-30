3bf5d9edddcbffcb3e845249b99b40b16df7e051
Treata <107116406+Treata11@users.noreply.github.com>
2022-09-30 14:00:26 +0330

    Initial commit

---
+++ Set/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Set
+//
+//  Created by Treata Norouzi on 9/30/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Treata Norouzi on 9/30/22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
