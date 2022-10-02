d0b5701cb6b3755281d8e31f7703ca39dbe833ad
wempaus <58611349+wempaus@users.noreply.github.com>
2022-10-02 17:20:02 +0330

    Add shapes: Oval, Rhombus; Create basic UI, Add AspectVGridFile

---
+++ Set/Shapes/Oval.swift
@@ -0,0 +1,25 @@
+//
+//  Oval.swift
+//  Set
+//
+//  Created by Treata Norouzi on 10/2/22.
+//
+
+import SwiftUI
+
+struct Oval: Shape {
+    
+    func path(in rect: CGRect) -> Path {
+        
+        let radius = min(rect.width, rect.height) / 2
+        
+        var p = Path()
+        
+        p.addArc(center: CGPoint(x: rect.midX, y: rect.minY + radius), radius: radius, startAngle: Angle(degrees: 180), endAngle: Angle(degrees: 0), clockwise: false)
+        p.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY - radius))
+        p.addArc(center: CGPoint(x: rect.midX, y: rect.maxY - radius), radius: radius, startAngle: Angle(degrees: 0), endAngle: Angle(degrees: 180), clockwise: false)
+        p.closeSubpath()
+
+        return p
+    }
+}
